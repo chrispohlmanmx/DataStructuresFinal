95ab7bf13e1109836ad1b22c108da317504c4a3a
chrispohlmanmx <c.pohlmanmx@gmail.com>
2023-04-02 12:36:25 -0500

    starting project

---
+++ ToDoPohlman.py
@@ -0,0 +1,18 @@
+"""
+* Name : ToDoPohlman.py
+* Author: Christopher Pohlman
+* Created : 4/2/2023
+* Course: CIS 152 - Data Structure
+* Version: 1.0
+* OS: Windows 11
+* IDE: PyCharm 
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Description : An app that .... ADD HERE....
+*            Input: ADD HERE XXX
+*            Output: ADD HERE XXX
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.
+"""
